b43f73271bab4b41b83976c50df59db6d39cdae8
JIAJIAXU <1923229320@qq.com>
2016-10-26 19:06:14 +0800

    filter-1

--- filter.py
+++ filter.py
@@ -7,3 +7,10 @@ def is_odd(n):
 
 
 print(list(filter(is_odd, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+
+
+def not_empty(s):
+    return s and s.strip()
+
+
+print(list(filter(not_empty, ['A', '', 'B', None, 'C', '  '])))
